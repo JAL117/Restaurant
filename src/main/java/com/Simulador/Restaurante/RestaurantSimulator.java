0347eee98ccf1da731e6ffd2b7ab2fce0539feda
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-11-27 14:10:08 -0600

    [UPDATE] main

--- src/main/java/com/Simulador/Restaurante/RestaurantSimulator.java
+++ src/main/java/com/Simulador/Restaurante/RestaurantSimulator.java
@@ -1,6 +1,44 @@
 package com.Simulador.Restaurante;
 
-import com.Simulador.Restaurante.entities.EntityType;
+import com.Simulador.Restaurante.business.services.RestauranteService;
+import com.Simulador.Restaurante.presentation.views.RestauranteView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RestaurantSimulator extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Crear instancia de RestauranteService
+        int capacidadMesas = 40; // Ajusta según necesidad
+        int cantidadMeseros = 4; // Ajusta según necesidad
+        int cantidadCocineros = 6; // Ajusta según necesidad
+
+        // Inicializar RestauranteService
+        RestauranteService restauranteService = new RestauranteService(capacidadMesas, cantidadMeseros, cantidadCocineros);
+
+        // Obtener la vista desde RestauranteService
+        RestauranteView view = restauranteService.getView();
+
+        // Configurar la escena con la vista
+        Scene scene = new Scene(view.getRoot());
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Simulador de Restaurante");
+        primaryStage.show();
+
+        // Iniciar la simulación
+        restauranteService.iniciarSimulacion();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+/*import com.Simulador.Restaurante.entities.EntityType;
 import com.Simulador.Restaurante.components.MainMenu;
 import com.Simulador.Restaurante.entities.RestaurantEntityFactory;
 import com.almasb.fxgl.app.GameApplication;
@@ -140,4 +178,4 @@ public class RestaurantSimulator extends GameApplication {
         imageView.setSmooth(true);
         return imageView;
     }
-}
+}*/
