9f58ea1a512bfac6cf845c6591b24a844dbe622e
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-11-27 11:59:02 -0600

    [ADD] Mesero actor on business

---
+++ src/main/java/com/Simulador/Restaurante/business/actors/Mesero.java
@@ -0,0 +1,13 @@
+package com.Simulador.Restaurante.business.actors;
+
+public class Mesero {
+    private final int id;
+
+    public Mesero(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
