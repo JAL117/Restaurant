fd15e29e408dbd8c1a205ee0aa4f6996193d0921
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-11-27 13:38:41 -0600

    [ADD] Cocinero thread on concurrency

---
+++ src/main/java/com/Simulador/Restaurante/business/services/RestauranteServices.java
@@ -0,0 +1,117 @@
+package com.Simulador.Restaurante.business.services;
+
+import com.Simulador.Restaurante.business.actors.Cocinero;
+import com.Simulador.Restaurante.business.actors.Comensal;
+import com.Simulador.Restaurante.business.actors.Recepcionista;
+import com.Simulador.Restaurante.business.actors.Mesero;
+import com.Simulador.Restaurante.business.utils.PoissonDistribution;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestauranteService {
+    private final MesaMonitor mesaMonitor;
+    private final OrdenMonitor ordenMonitor;
+    private final ComidaMonitor comidaMonitor;
+    private final ComensalMonitor comensalMonitor;
+    private final Recepcionista recepcionista;
+    private final List<Mesero> meseros;
+    private final List<Cocinero> cocineros;
+    private final List<Thread> threads;
+    private final RestauranteView view;
+    private volatile boolean running;
+    private int totalComensales = 0;
+    private int ordenesProcesadas = 0;
+
+    public RestauranteService(int capacidad, int cantidadMeseros, int cantidadCocineros) {
+        this.mesaMonitor = new MesaMonitor(capacidad);
+        this.ordenMonitor = new OrdenMonitor();
+        this.comidaMonitor = new ComidaMonitor();
+        this.comensalMonitor = new ComensalMonitor();
+        this.recepcionista = new Recepcionista();
+        this.meseros = new ArrayList<>();
+        this.cocineros = new ArrayList<>();
+        this.threads = new ArrayList<>();
+        this.view = new RestauranteView(mesaMonitor.getMesas());
+        this.running = false;
+
+        for (int i = 1; i <= cantidadMeseros; i++) {
+            meseros.add(new Mesero(i));
+        }
+
+        for (int i = 1; i <= cantidadCocineros; i++) {
+            cocineros.add(new Cocinero(i));
+        }
+    }
+
+    public void iniciarSimulacion() {
+        if (view == null) {
+            throw new IllegalStateException("La vista no ha sido inicializada correctamente");
+        }
+        running = true;
+
+        view.setTotalCocineros(cocineros.size());
+
+        for (Mesero mesero : meseros) {
+            view.añadirMesero(mesero.getId());
+        }
+
+        for (Cocinero cocinero : cocineros) {
+            view.añadirCocinero(cocinero.getId());
+        }
+
+        for (Mesero mesero : meseros) {
+            MeseroThread meseroThread = new MeseroThread(mesero, ordenMonitor, comidaMonitor, comensalMonitor, view, this);
+            threads.add(meseroThread);
+            meseroThread.start();
+        }
+
+        for (Cocinero cocinero : cocineros) {
+            CocineroThread cocineroThread = new CocineroThread(cocinero, ordenMonitor, comidaMonitor, view, this);
+            threads.add(cocineroThread);
+            cocineroThread.start();
+        }
+
+        Thread generadorComensales = new Thread(() -> {
+            int comensalId = 1;
+            while (running) {
+                try {
+                    double lambda = 1.0;
+                    double tiempoEntreLlegadas = PoissonDistribution.generarTiempoLlegada(lambda);
+                    Thread.sleep((long) (tiempoEntreLlegadas * 1000));
+
+                    Comensal comensal = new Comensal(comensalId++);
+                    ComensalThread comensalThread = new ComensalThread(comensal, recepcionista, mesaMonitor, comensalMonitor, view, ordenMonitor);
+                    threads.add(comensalThread);
+                    comensalThread.start();
+
+                    totalComensales++;
+                    view.actualizarTotalComensales(totalComensales);
+
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        generadorComensales.start();
+        threads.add(generadorComensales);
+    }
+
+    public void finalizarSimulacion() {
+        running = false;
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+        System.out.println("Simulación finalizada.");
+    }
+
+    public synchronized void incrementarOrdenesProcesadas() {
+        ordenesProcesadas++;
+        view.actualizarOrdenesProcesadas(ordenesProcesadas);
+    }
+
+    public RestauranteView getView() {
+        return view;
+    }
+}
---
+++ src/main/java/com/Simulador/Restaurante/concurrency/threads/CocineroThread.java
@@ -0,0 +1,45 @@
+package com.Simulador.Restaurante.concurrency.threads;
+
+import com.Simulador.Restaurante.business.actors.Cocinero;
+import com.Simulador.Restaurante.business.models.Orden;
+import com.Simulador.Restaurante.concurrency.monitors.ComidaMonitor;
+import com.Simulador.Restaurante.concurrency.monitors.OrdenMonitor;
+import com.Simulador.Restaurante.presentation.views.RestauranteView;
+import com.Simulador.Restaurante.business.services.RestauranteService;
+
+public class CocineroThread extends Thread {
+    private final Cocinero cocinero;
+    private final OrdenMonitor ordenMonitor;
+    private final ComidaMonitor comidaMonitor;
+    private final RestauranteView view;
+    private final RestauranteService service;
+
+    public CocineroThread(Cocinero cocinero, OrdenMonitor ordenMonitor, ComidaMonitor comidaMonitor, RestauranteView view, RestauranteService service) {
+        this.cocinero = cocinero;
+        this.ordenMonitor = ordenMonitor;
+        this.comidaMonitor = comidaMonitor;
+        this.view = view;
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Orden orden = ordenMonitor.retirarOrden();
+                System.out.println("Orden " + orden.getId() + " retirada del buffer de órdenes.");
+                System.out.println("Cocinero " + cocinero.getId() + " está preparando la Orden " + orden.getId());
+
+                Thread.sleep(3000);
+
+                comidaMonitor.agregarComida(orden);
+                System.out.println("Cocinero " + cocinero.getId() + " ha terminado de preparar la Orden " + orden.getId());
+                System.out.println("Comida " + orden.getId() + " añadida al buffer de comidas.");
+
+                service.incrementarOrdenesProcesadas();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
