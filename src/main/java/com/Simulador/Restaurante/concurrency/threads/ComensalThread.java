d60a492ab9915bf2577a243e3e9dbff830f62fbf
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-11-27 13:48:57 -0600

    [ADD] Mesero thread on concurrency

--- src/main/java/com/Simulador/Restaurante/concurrency/threads/ComensalThread.java
+++ src/main/java/com/Simulador/Restaurante/concurrency/threads/ComensalThread.java
@@ -29,42 +29,32 @@ public class ComensalThread extends Thread {
     @Override
     public void run() {
         try {
-            // El comensal llega al restaurante
             view.moverComensalAEntrada(comensal.getId());
-            Thread.sleep(500); // Espera un poco antes de ir a la recepción
+            Thread.sleep(500);
 
-            // El comensal va a la recepción
             view.moverComensalARecepcion(comensal.getId());
             System.out.println("Recepcionista está registrando al Comensal " + comensal.getId());
 
-            // Asignar mesa
             Mesa mesa = mesaMonitor.asignarMesa(comensal);
             recepcionista.asignarMesa(comensal, mesa);
             System.out.println("Recepcionista ha asignado la Mesa " + mesa.getNumero());
 
-            // Actualizar vista
             view.actualizarEstadoMesa(mesa.getNumero(), mesa.getEstado());
 
-            // El comensal va a su mesa
             view.moverComensalAMesa(comensal.getId(), mesa.getNumero());
 
-            // Añadir el comensal a la cola de espera de mesero
             comensalMonitor.agregarComensal(comensal);
             System.out.println("Comensal " + comensal.getId() + " añadido a la cola de espera.");
 
-            // Esperar a que el mesero tome su orden y la comida sea servida
             synchronized (comensal) {
                 comensal.wait();
             }
 
-            // El comensal come durante un tiempo
-            Thread.sleep(5000); // Simula el tiempo que tarda en comer
+            Thread.sleep(5000);
 
-            // El comensal se va del restaurante
             System.out.println("Comensal " + comensal.getId() + " ha terminado de comer y se va.");
             view.moverComensalFuera(comensal.getId());
 
-            // Liberar mesa
             mesaMonitor.liberarMesa(mesa);
             view.actualizarEstadoMesa(mesa.getNumero(), mesa.getEstado());
 
