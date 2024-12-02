5b07ba83f2a52501d1eddddf4abbe4a7c62c4a4b
Jal <106935663+JAL117@users.noreply.github.com>
2024-12-02 00:09:13 -0600

    .

--- src/main/java/com/Simulador/Restaurante/RestaurantSimulator.java
+++ src/main/java/com/Simulador/Restaurante/RestaurantSimulator.java
@@ -1,21 +1,12 @@
 package com.Simulador.Restaurante;
-
 import com.Simulador.Restaurante.business.services.RestauranteService;
-import com.Simulador.Restaurante.business.utils.PoissonDistribution;
 import com.Simulador.Restaurante.presentation.views.RestauranteView;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.texture.Texture;
-import javafx.scene.Scene;
-import com.Simulador.Restaurante.business.utils.RandomUtils;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.File;
-import java.util.Objects;
-
-import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameScene;
 
 public class RestaurantSimulator extends GameApplication {
 
@@ -32,21 +23,15 @@ public class RestaurantSimulator extends GameApplication {
     }
 
 
-
-
-
-
     @Override
     protected void initGame() {
-        // Crear instancia del servicio y vista
+
         int capacidadMesas = 20;
         int cantidadMeseros = (int) Math.round(capacidadMesas * 0.10);
         int cantidadCocineros = (int) Math.round(capacidadMesas * 0.15);
 
         restauranteService = new RestauranteService(capacidadMesas, cantidadMeseros, cantidadCocineros);
         restauranteView = restauranteService.getView();
-
-        // Cargar la textura del fondo
 
 
         Image backgroundImage = new Image(getClass().getResource("/assets/textures/fondo.png").toExternalForm());
@@ -57,7 +42,6 @@ public class RestaurantSimulator extends GameApplication {
         FXGL.getGameScene().getContentRoot().getChildren().add(0, backgroundImageView);
 
 
-        // Iniciar simulación y agregar vista
         restauranteService.iniciarSimulacion();
         FXGL.getGameScene().addUINode(restauranteView.getRoot());
     }
