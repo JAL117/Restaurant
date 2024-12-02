7668d4d03ea923cc5c2f169b58100921d07b93db
Jal <106935663+JAL117@users.noreply.github.com>
2024-12-02 09:17:19 -0600

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.example.restaurant {
     requires com.almasb.fxgl.all;
 
     opens com.Simulador.Restaurante to javafx.fxml;
-    opens com.Simulador.Restaurante.entities to com.almasb.fxgl.core;
+
 
     exports com.Simulador.Restaurante;
 }
